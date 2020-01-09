c7cc7d72b1605caf727e9eaf81ce6656d75b753f
clintg105 <clintengraham@gmail.com>
2020-01-09 10:21:14 -0600

    Cross-Platform file names

--- python/utilities/PathMaker.py
+++ python/utilities/PathMaker.py
@@ -1,18 +1,24 @@
 import os
 import sys
+from platform import system
 
 LEVELS = 1
 class PathMan():
     def __init__(self,levels = LEVELS):
         os.chdir(os.path.abspath(os.path.split(sys.argv[0])[0]))
         cwd = os.getcwd()
-        #print("FOLDERSEARCHER:",cwd)
+        # print("FOLDERSEARCHER:", cwd)
+        self.split_str = {
+            'Windows': '\\',
+            'Linux': '/',
+            'Darwin': '/'  # Mac
+        }[system()]  # Clint - added this attribute to fix some cross-platforming issues
         self.levels = levels
         self.path = None
         self._pathCutter(cwd)
 
     def _pathCutter(self,string):
-        lister = string.split("\\")
+        lister = string.split(self.split_str)
 
         for i in range(len(lister)):
             if lister[i]=="python":
@@ -24,7 +30,7 @@ class PathMan():
 
         path = ""
         for i in range(j):
-            path = path + lister[i] + "\\"
+            path = path + lister[i] + self.split_str
         self.path = path
         #print(path)
 
