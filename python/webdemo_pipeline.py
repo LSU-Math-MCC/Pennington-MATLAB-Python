f234955f4fc1da6bc0b34f66003073b2be3f6035
clintg105 <clintengraham@gmail.com>
2020-02-02 20:43:17 -0600

    Using cname lists

--- python/webdemo_pipeline.py
+++ python/webdemo_pipeline.py
@@ -12,9 +12,10 @@ from ml_core import run_batch
 '''
 Create DataSet from CSV
     Here we import a ShapeUp excel file into a pandas DataFrame and use to_DataSet to combine the digital anthropometric
-data with DXA and blood work target data in an instance of the DataSet class. Typically DataSets are imported from 
-DataSets.py, but in this file we have chosen to illustrate the pipeline starting directly from the output of the MATLAB 
-code (an 'ObjOrganizer' xlsx spreadsheet).
+data with DXA and blood work target data in an instance of the DataSet class. 
+Note: We have cleaned up the data and typically DataSets are imported from DataSets.py, So you DON'T need to do this step for OUR datasets!
+However, in this file we have illustrated the pipeline starting directly from the output of the MATLAB 
+code (an 'ObjOrganizer' xlsx spreadsheet) so the developer learns how to create Datasets if a researcher uploads a new set of data.
 '''
 df = pd.read_excel('data/ShapeUp/ObjOrganizerStyku_v14.xlsx')
 df = df.drop(df.columns[[0, 1, 3, 4]], axis=1)  # remove unnecessary label columns
@@ -31,43 +32,46 @@ passed to our ML model. The options for extract_data are given in the data_confi
 '''
 data_config_dict = {
     # dictionary containing information on how our DataSet should be extracted and scaled.
-    'target_cnames': ['TOTAL_FAT', 'TOTAL_LEAN'],
+    'target_cnames': ['TOTAL_FAT', 'TOTAL_LEAN'], # this is an example for two targets, you can pick any "cname" to be your target
     'feature_options': {
-        # collections of features to preform a grid search over
-        "bmi": {
-            "N": [],
-            "Y": ["BMI1"]
-        },
-        "age": {
-            # "N": [],
-            "Y": ["age"]
-        },
-        "sex": {
-            # "N": [],
-            "Y": ["SEX"],
-        },
-        "volumes": {
-            # "N": [],
-            "Y": ["TotalVolume", "headVolume", "rArmVolume", "lArmVolume", "rLegVolume", "lLegVolume",
-                  "trunkVolume"]
-        },
-        "measurements": {
-            "none": [],
-            "common": ["waist circ", "hip circ", "rThighGirth", "rbicepGirth"],
-            # "all": ["Chest circ", "waist circ", "hip circ", "rThighGirth", "lThighGirth",
-            #         "rCalfCirc", "lCalfCirc", "rWristGirth", "lWristGirth", "rForearm",
-            #         "lForearmGirth", "rbicepGirth", "lBicepGirth", "rAnkle", "Lankle",
-            #         "LarmLength", "RArmLength", "CollarScalp", "TrunkLength", "lLegLength", "rLegLength",
-            #         "crotchHeight"],
-            # "rAnkle" and "Lankle" are ankle girths
-        },
-        "a_b": {
-            "none": [],
-            # "four": ["waist circ A_B", "hip circ A_B", "rThighGirth A_B", "rbicepGirth A_B"],
-            "all": ["Chest circ A_B", "waist circ A_B", "hip circ A_B", "rThighGirth A_B", "lThighGirth A_B",
-                    "rCalfCirc A_B", "lCalfCirc A_B", "lWristGirth A_B",
-                    "rForearm A_B", "lForearmGirth A_B", "rbicepGirth A_B", "lBicepGirth A_B", "rAnkle A_B",
-                    "rWristGirth A_B", "Lankle A_B"]
+        # # collections of features to preform a grid search over
+        # "bmi": {
+        #     "N": [],
+        #     "Y": ["BMI1"]
+        # },
+        # "age": {
+        #     # "N": [],
+        #     "Y": ["age"]
+        # },
+        # "sex": {
+        #     # "N": [],
+        #     "Y": ["SEX"],
+        # },
+        # "volumes": {
+        #     # "N": [],
+        #     "Y": ["TotalVolume", "headVolume", "rArmVolume", "lArmVolume", "rLegVolume", "lLegVolume",
+        #           "trunkVolume"]
+        # },
+        # "measurements": {
+        #     "none": [],
+        #     "common": ["waist circ", "hip circ", "rThighGirth", "rbicepGirth"],
+        #     # "all": ["Chest circ", "waist circ", "hip circ", "rThighGirth", "lThighGirth",
+        #     #         "rCalfCirc", "lCalfCirc", "rWristGirth", "lWristGirth", "rForearm",
+        #     #         "lForearmGirth", "rbicepGirth", "lBicepGirth", "rAnkle", "Lankle",
+        #     #         "LarmLength", "RArmLength", "CollarScalp", "TrunkLength", "lLegLength", "rLegLength",
+        #     #         "crotchHeight"],
+        #     # "rAnkle" and "Lankle" are ankle girths
+        # },
+        # "a_b": {
+        #     "none": [],
+        #     # "four": ["waist circ A_B", "hip circ A_B", "rThighGirth A_B", "rbicepGirth A_B"],
+        #     "all": ["Chest circ A_B", "waist circ A_B", "hip circ A_B", "rThighGirth A_B", "lThighGirth A_B",
+        #             "rCalfCirc A_B", "lCalfCirc A_B", "lWristGirth A_B",
+        #             "rForearm A_B", "lForearmGirth A_B", "rbicepGirth A_B", "lBicepGirth A_B", "rAnkle A_B",
+        #             "rWristGirth A_B", "Lankle A_B"]
+        # },
+        "Features": {
+            'List1': ['rArmVolume']
         }
     },
     'transform_options': {
@@ -96,13 +100,14 @@ pd.set_option('display.width', 1000)
 
 
 '''
-Example Run 1: Using a list of regressors
+Example Run 1: experimenting with a list of regressors to find best regressor for the data
     By default, only one example run in this file should be uncommented. Tip: to (un)comment lines in bulk using 
 PyCharm, highlight the desired lines of code an press ctrl + '/'.
 '''
 print('EXAMPLE RUN 1: Using a list of regressors')
 
 regressors = [
+# In this example we are trying out three different regressors: Linear, Lasso, MLP.
     LinearRegression(),
     Lasso(alpha=.001),
     MLPRegressor(solver="lbfgs",
@@ -113,13 +118,13 @@ regressors = [
 
 results = run_batch(
     # pass required arguments
-    StykuDataset,
-    data_config_dict,
-    regressors,
+    StykuDataset, # dataset created using MyDataset
+    data_config_dict, # Option for data extraction and grouping based on dataset created by MyDataset
+    regressors, # regression method and hyperparameter
     # configure internal parameters for n-fold cross-validation and scoring
     cv_params=dict(
         n_splits=5,
-        scorers=['r2', 'explained_variance']  # options: https://scikit-learn.org/stable/modules/model_evaluation.html
+        scorers=['r2', 'explained_variance']  # options for different scores can be found at https://scikit-learn.org/stable/modules/model_evaluation.html
     ),
     # print the best 3 runs (sorted by test_r2) for each target and sex option
     show_best_runs=3
@@ -128,11 +133,12 @@ print('[RESULTS]\n', results)
 
 
 '''
-Example Run 2: Using a single regressor with a hyper-parameter grid search
+Example Run 2: Using a single regressor (the best regressor selected from "Example Run 1") 
+and experimenting with different hyper-parameter values by a grid search to do hyperparameter tuning
 '''
 # print('EXAMPLE RUN 2: Using a single regressor with a hyper-parameter grid search')
 #
-# regressor = MLPRegressor
+# regressor = MLPRegressor # assuming MLP was the best regressor based on EXample Run 1 outcomes
 # regressor_param_dict = dict(
 #     solver=["adam", "lbfgs"],
 #     activation=["identity", "tanh"],
@@ -144,7 +150,9 @@ Example Run 2: Using a single regressor with a hyper-parameter grid search
 #     StykuDataset,
 #     data_config_dict,
 #     regressor,
-#     regressor_param_dict,
+#     regressor_param_dict, # in this example in addition to the regressor type,
+#                           #  we give a regressor hyperparameter dictionary as
+#                           #  we are doing a grid search to find best parameter.
 #     show_best_runs=3
 # )
 # print('[RESULTS]\n', results)
